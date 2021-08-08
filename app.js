08b79d7aae2ada188b23bdb81e0a97fb82cda5de
Raven Ling <87859100+tree12132002@users.noreply.github.com>
2021-08-08 20:28:06 +0800

    use dynamic data in index page

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 
-app.engine('handlebars', exphbs ({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results })
 })
 
 app.listen(port, () => {
