a09d0c1d18c89b3a77f165cf704c78a7d782bdb9
Raven Ling <87859100+tree12132002@users.noreply.github.com>
2021-08-08 23:54:34 +0800

    final check

---
+++ README.md
@@ -0,0 +1,35 @@
+# Restaurant-List
+
+## 功能
+1. 使用者可以在首頁看到所有餐廳與它們的簡單資料
+2. 使用者可以再點進去看餐廳的詳細資訊
+3. 使用者可以透過搜尋餐廳名稱來找到特定的餐廳
+## 使用方法（兩種下載法二擇一）
+1. 使用cmd下載：
+    ```
+    
+    ```
+2. 簡易下載：
+   I. 點選綠色框框的『Code』
+   II. 點選『Download ZIP』
+3. 打開cmd，將路徑切至資料夾
+   I. 安裝套件
+    ```
+    npm install
+    ```
+    II. 輸入指令來執行
+    ```
+    npm run dev
+    ```
+4. 使用`ctrl + c`或`cmd + c`離開
+## 相關套件與版本
+##### 檔案管理相關
+* npm：7.7.6
+* express: 4.17.1
+* express handlebars: 5.3.2
+* nodemon: 2.0.9
+##### 視覺效果相關
+* jquery: 3.6.0
+* popper: v2.9.1
+* bootstrap: v4.6.0
+* font-awesome: v5.15.3
--- app.js
+++ app.js
@@ -1,36 +1,40 @@
+// require packages used in the project
 const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+const restaurantList = require('./restaurant.json').results
 
+// setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// setting static files
 app.use(express.static('public'))
 
+// routes setting
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
+  res.render('index', { restaurants: restaurantList })
 })
-
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant =>
+  const restaurant = restaurantList.find(restaurant =>
     restaurant.id.toString() === req.params.restaurant_id)
 
   res.render('show', { restaurant: restaurant })
 })
-
 app.get('/search', (req, res) => {
-  console.log(req.query.keyword)
   const keyword = req.query.keyword
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
-      restaurant.category.toLowerCase().includes(keyword.toLowerCase())
-  })
+  const restaurants = restaurantList.filter(restaurant =>
+    restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+    restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
 
-  res.render('index', { restaurants: restaurants })
+  if (!restaurants.length) {
+    res.render('noresult')
+  }
+  res.render('index', { restaurants: restaurants, keyword: keyword })
 })
 
+// start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
