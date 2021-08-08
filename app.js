4bc6ec6b387a2c2e0c636f4d9006c269ae09aab2
Raven Ling <87859100+tree12132002@users.noreply.github.com>
2021-08-08 21:46:55 +0800

    add show page

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => 
+    restaurant.id.toString() === req.params.restaurant_id)
+
+  res.render('show', { restaurant: restaurant })
+})
+
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
