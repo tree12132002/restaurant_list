52be9767677c80ca67ebbef785556319cab2ee05
Raven Ling <87859100+tree12132002@users.noreply.github.com>
2021-08-16 06:09:31 +0800

    add delete function

--- app.js
+++ app.js
@@ -103,7 +103,14 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
-
+// delete a restaurant
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 
 app.get('/search', (req, res) => {
