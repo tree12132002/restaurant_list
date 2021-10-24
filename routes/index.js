360382f1bcca2f2ceca63319284ce520dbdab3fb
tree12132002 <tree12132002@hotmail.com>
2021-10-24 19:15:26 +0800

    add user modules

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
+
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
+const users = require('./modules/users')
 // 準備引入路由模組
 router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/search', search)
+router.use('/users', users)
 // 匯出路由器
+
 module.exports = router
