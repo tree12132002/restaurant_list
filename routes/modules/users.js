723d81c43a1682286e101914c5554c3f05b9cb7f
yourname <tree12132002@hotmail.com>
2021-10-25 02:51:50 +0800

    small fix

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -11,7 +11,7 @@ router.get('/login', (req, res) => {
 // login function 加入 middleware，驗證 request 登入狀態
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: '/users/login'
+  failureRedirect: 'users/login'
 }))
 // enter register page
 router.get('/register', (req, res) => {
