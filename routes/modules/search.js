0a4135298fd28524eb1cbd087f9bc12f2b7e75fd
yourname <tree12132002@hotmail.com>
2021-10-26 08:06:50 +0800

    modify search function

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -4,9 +4,10 @@ const Restaurant = require('../../models/restaurant')
 
 // search results routes setting
 router.get('/', (req, res) => {
+  const userId = req.user._id
   const keyword = req.query.keyword.trim().toLowerCase()
   const keywordRegex = new RegExp(keyword, 'i')
-  Restaurant.find({ $or: [{ category: { $regex: keywordRegex } }, { name: { $regex: keywordRegex } }] })
+  Restaurant.find({ userId, $or: [{ category: { $regex: keywordRegex } }, { name: { $regex: keywordRegex } }] })
     .lean()
     .then(restaurants => {
       res.render('index', { restaurants, keyword })
